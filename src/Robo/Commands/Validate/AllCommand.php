641efe75a6706ecb1789500e3075ea5599cd313e
Luca Cracco <craccoluca@gmail.com>
2017-09-06 20:40:05 +0200

    Init CommandValidation - no complete

---
+++ src/Robo/Commands/Validate/AllCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Lucacracco\RoboDrupal8\Robo\Commands\Validate;
+
+use Lucacracco\RoboDrupal8\Robo\RoboDrupal8Tasks;
+
+/**
+ * Defines commands in the "validate:all*" namespace.
+ */
+class AllCommand extends RoboDrupal8Tasks {
+
+  /**
+   * Runs all code validation commands.
+   *
+   * @command validate
+   *
+   * @aliases validate:all
+   */
+  public function all() {
+    $status_code = $this->invokeCommands([
+      'validate:composer',
+      'validate:phpcs',
+    ]);
+
+    return $status_code;
+  }
+
+}
--- src/Robo/RoboDrupal8Tasks.php
+++ src/Robo/RoboDrupal8Tasks.php
@@ -14,6 +14,7 @@ use Robo\Contract\ConfigAwareInterface;
 use Robo\Contract\IOAwareInterface;
 use Robo\LoadAllTasks;
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\ArrayInput;
 
 /**
  * Base class for RD8 Robo commands.
@@ -46,6 +47,67 @@ class RoboDrupal8Tasks implements ConfigAwareInterface, LoggerAwareInterface, Bu
     $table->setHeaders($headers)
       ->setRows(ArrayManipulator::convertArrayToFlatTextArray($array))
       ->render();
+  }
+
+  /**
+   * The depth of command invocations, used by invokeCommands().
+   *
+   * E.g., this would be 1 if invokeCommands() called a method that itself
+   * called invokeCommands().
+   *
+   * @var int
+   */
+  protected $invokeDepth = 0;
+
+  /**
+   * Invokes an array of Symfony commands.
+   *
+   * @param array $commands
+   *   An array of Symfony commands to invoke. E.g., 'tests:behat'.
+   */
+  protected function invokeCommands(array $commands) {
+    foreach ($commands as $key => $value) {
+      if (is_numeric($key)) {
+        $command = $value;
+        $args = [];
+      }
+      else {
+        $command = $key;
+        $args = $value;
+      }
+      $this->invokeCommand($command, $args);
+    }
+  }
+
+  /**
+   * Invokes a single Symfony command.
+   *
+   * @param string $command_name
+   *   The name of the command. E.g., 'tests:behat'.
+   * @param array $args
+   *   An array of arguments to pass to the command.
+   *
+   * @throws \Exception
+   */
+  protected function invokeCommand($command_name, array $args = []) {
+    $this->invokeDepth++;
+
+    /** @var \Lucacracco\RoboDrupal8\Robo\Application $application */
+    $application = $this->getContainer()->get('application');
+    $command = $application->find($command_name);
+
+    $input = new ArrayInput($args);
+    $prefix = str_repeat(">", $this->invokeDepth);
+    $this->output->writeln("<comment>$prefix $command_name</comment>");
+    $exit_code = $application->runCommand($command, $input, $this->output());
+    $this->invokeDepth--;
+
+    // The application will catch any exceptions thrown in the executed
+    // command. We must check the exit code and throw our own exception. This
+    // obviates the need to check the exit code of every invoked command.
+    if ($exit_code) {
+      throw new \Exception("Command `$command_name {$input->__toString()}` exited with code $exit_code.");
+    }
   }
 
 }
